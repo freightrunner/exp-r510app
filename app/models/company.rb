ca1de573b14faadf360f547e19f311f15b863e9e
Brian Arpaio <arpaio@gmail.com>
2017-05-07 18:49:00 -0400

    Company must have name before saving

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,7 @@
 class Company < ApplicationRecord
   belongs_to :user, optional: true
 
+  validates :name, presence: true
+
   enum status: [ :is_available, :is_lead, :do_not_call, :is_contracted ]
 end
