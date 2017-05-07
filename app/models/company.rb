7d9db3eacd0e5b6e61271e02eb9d7c3910ade085
Brian Arpaio <arpaio@gmail.com>
2017-05-07 18:53:42 -0400

    Company must have website address
    Test now passing

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ApplicationRecord
   belongs_to :user, optional: true
 
   validates :name, presence: true
+  validates :website_address, presence: true
 
   enum status: [ :is_available, :is_lead, :do_not_call, :is_contracted ]
 end
