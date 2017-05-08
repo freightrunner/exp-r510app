3ff671a51abfa0f631127d2a89811a236befe183
Brian Arpaio <arpaio@gmail.com>
2017-05-08 08:04:42 -0400

    Created test to format company.phone_number

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class CompanyTest < ActiveSupport::TestCase
+	test "Company phone number has all dashes and dots removed" do
+		comp = Company.new(name: "A company", website_address: "xyz.com", phone_number: "123-456-7890")
+		comp.save
+		assert_equal "1234567890", comp.phone_number
+	end
   # test "the truth" do
   #   assert true
   # end
