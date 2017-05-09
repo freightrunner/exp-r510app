c6019c4cae4fcabd2d21ac9401b69530cdfc6b9d
Brian Arpaio <arpaio@gmail.com>
2017-05-09 19:37:41 -0400

    Created test for removing user if company.status is updated to 0

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -7,4 +7,15 @@ class CompanyTest < ActiveSupport::TestCase
 		assert_equal "1234567890", comp.phone_number
 	end
 
+	test "if company status is open then user is removed" do
+		@user = User.new
+		@user.save
+		comp = Company.new(name: "A Company", website_address: "somewebsite.org", phone_number: "1234567890", user: nil, status: 0)
+		comp.save
+		assert_equal "is_available", comp.status
+		comp.update!(user: @user, status: 1)
+		assert_equal @user, comp.user
+		refute comp.update!(status: 0), "Comapny updated to status 0 saved with User"
+	end
+
 end
