a5aac947ae7a705f83574288d5de737b3eea2856
Brian Arpaio <arpaio@gmail.com>
2017-05-10 23:32:47 +0000

    test for telephone number formatting working for contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,11 @@
 class Contact < ApplicationRecord
   belongs_to :company
+  
+  before_validation :format_contact_numbers
+  
+  private
+    def format_contact_numbers
+      self.office_phone_number = office_phone_number.gsub(/[^0-9]/, "") if attribute_present?("office_phone_number")
+      self.cell_phone_number = cell_phone_number.gsub(/[^0-9]/, "") if attribute_present?("cell_phone_number")
+    end
 end
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @contact = contacts(:one)
+  end
+  
+  test "all phone numbers have hypens and other characters removed" do
+    @contact.save
+    assert_equal "5555555555", @contact.office_phone_number
+    assert_equal "6666666666", @contact.cell_phone_number
+  end
 end
