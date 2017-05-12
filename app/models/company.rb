e9788ac3e50f7b7ab6df2f3cb71afce3b7bc384d
Brian Arpaio <arpaio@gmail.com>
2017-05-12 01:25:55 +0000

    added Contant in company.rb to ensure that segment_category fits company standards and then validation to ensure that user uses contant values

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,15 +1,20 @@
 class Company < ApplicationRecord
   belongs_to :user, optional: true
   has_many :contacts, dependent: :destroy
+  
+  CATEGORIES = ['Agriculture/Environment', 'Architecture', 'Arts & Culture', 'Business', 'Culinary', 'Education', 'Health/Medical', 'Outdoor', 'Travel']
 
   validates :name, presence: true
   validates :website_address, presence: true
+  validates :segment_category, inclusion: { :in => CATEGORIES,
+                                          message: "%{value} is not a valid segment category, please select from the list" }
 
   before_validation :format_phone_number
   before_save :remove_user, if: :company_is_available?
 
 
   enum status: [ :is_available, :is_lead, :do_not_call, :is_contracted ]
+  
 
   def company_is_available?
   	self.is_available?
