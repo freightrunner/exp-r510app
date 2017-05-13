86bf4efd9344da073385674c0c7404198d19d966
freightrunner <brian@rpayo.com>
2017-05-13 12:14:42 -0400

    updated account links in navbar, edited routes to make contacts nested of companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :contacts
-  resources :companies
+  
+  resources :companies do
+  	resources :contacts
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "companies#index"
